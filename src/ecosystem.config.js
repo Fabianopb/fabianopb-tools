8bd4db819dfd7a5dcabbe24fc354f6890513f1b2
Fabiano Brito <fabiano.brito@wolt.com>
2022-12-22 16:42:19 -0300

    Add pm2 ecosystem file

---
+++ src/ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'danik',
+      script: 'danik-dist/index.js',
+      watch: true,
+      autorestart: false,
+    },
+    {
+      name: 'fabiano',
+      script: 'fpb-dist/server/index.js',
+      watch: true,
+      autorestart: false,
+    },
+    {
+      name: 'mongo-backup',
+      script: 'tools/backupMongo/index.js',
+      watch: false,
+      autorestart: false,
+      instances: 1,
+      cron_restart: '0,5 3 * * Sunday,Monday',
+    },
+  ],
+};
